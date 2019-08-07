90921ed16ca973b06446f0c01b8f26a2f2010178
Ubuntu <ubuntu@ip-172-31-88-175.ec2.internal>
2019-08-07 18:57:17 +0000

    css for login

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from google.appengine.ext import ndb
 
 class Event(ndb.Model):
+    owner = ndb.StringProperty(required=True)
     host = ndb.StringProperty(required=True)
     guest = ndb.StringProperty(required=True)
     date = ndb.StringProperty(required=True)
