62b4f40d86504ab2d64ffe40f8de6c0d520ccdf7
Ubuntu <ubuntu@ip-172-31-88-175.ec2.internal>
2019-08-08 16:40:44 +0000

    created new py file for twilio API

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import jinja2
 import os
 from models import Event, CrashCouchUser
 from google.appengine.api import users
-from twilio.rest import Client
+
 
 
 
@@ -13,19 +13,7 @@ the_jinja_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
     
-def send_mess():   
-        account_sid = 'ACeb53036e0973b4d06165a904bb49b64a'
-        auth_token = '36fb7e99c2b6eb9fc699a7f9892e210c'
-        client = Client(account_sid, auth_token)
-        
-        message = client.messages \
-                        .create(
-                             body="hello this crash couch we are watching you and it seems you have not signed up for our website",
-                             from_='+18317848769',
-                             to='+18316822956'
-                         )
-        
-        print(message.sid)
+
 
 
 def checkLoggedInAndRegistered(request):
